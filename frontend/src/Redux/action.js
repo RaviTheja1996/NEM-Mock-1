ac5d94e7b8f82cf429f642404e099284ad9daf8e
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-12-05 00:41:20 +0530

    console.log() in action.js delete method

--- frontend/src/Redux/action.js
+++ frontend/src/Redux/action.js
@@ -14,7 +14,9 @@ export const DeleteContact = (obj) => async (dispatch) => {
   axios.delete(
     `https://giddy-raincoat-bee.cyclic.app/users/delete?id=${obj._id}`,
     obj
-  ).then((res) => { dispatch({ type: DELETE_USER, payload: obj }); console.log("inside dot then of delete method in action.js"); console.log(res.data); }).catch((err) => console.log(err.message));
+  )
+    .then((res) => { dispatch({ type: DELETE_USER, payload: obj }); console.log("inside dot then of delete method in action.js"); console.log(res.data); })
+    .catch((err) => { console.log(err.message); console.log("inside action.js delete method axios catch block") });
 };
 
 export const EditContact = (obj) => async (dispatch) => {
