d6bd6c511b22a004cc8f31a692d5b6984d089069
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-12-04 19:21:40 +0530

    altered update route

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -10,20 +10,20 @@ userRouter.post("/contacts", async (req, res) => {
     try {
         const user = new UserModel({ name: username, email, phone, label });
         const new_user = await user.save();
-        res.status(200).send({ "msg": "the user is registered successfully", "newUser": new_user });
-    }
-    catch (err) {
-        res.status(500).send({ "Error": err.message });
+        res
+            .status(200)
+            .send({ msg: "the user is registered successfully", newUser: new_user });
+    } catch (err) {
+        res.status(500).send({ Error: err.message });
     }
 });
 
 userRouter.get("/", async (req, res) => {
     try {
         const contacts = await UserModel.find();
-        res.status(200).send({ "contacts": contacts });
-    }
-    catch (err) {
-        res.status(500).send({ "Error": err.message });
+        res.status(200).send({ contacts: contacts });
+    } catch (err) {
+        res.status(500).send({ Error: err.message });
     }
 });
 
@@ -31,22 +31,26 @@ userRouter.get("/search", async (req, res) => {
     const { name } = req.params;
     try {
         const result = await UserModel.find({ name });
-        res.status(200).send({ "employee": result });
+        res.status(200).send({ employee: result });
+    } catch (err) {
+        res.status(500).send({ Error: err.message });
     }
-    catch (err) {
-        res.status(500).send({ "Error": err.message });
-    }
-})
+});
 
 userRouter.patch("/update", async (req, res) => {
     const { name } = req.params;
     try {
-        const user = UserModel.findOne({ name });
-        const updated_user = await UserModel.findByIdAndUpdate({ _id: user._id }, req.body);
-        res.status(200).send({ "msg": `user with id ${user._id} is updated successfully`, "updated_user": updated_user });
-    }
-    catch (err) {
-        res.status(500).send({ "Error": err.message });
+        const user = UserModel.findOne({ email: name });
+        const updated_user = await UserModel.findByIdAndUpdate(
+            { _id: user._id },
+            req.body
+        );
+        res.status(200).send({
+            msg: `user with id ${user._id} is updated successfully`,
+            updated_user: updated_user,
+        });
+    } catch (err) {
+        res.status(500).send({ Error: err.message });
     }
 });
 
@@ -55,11 +59,13 @@ userRouter.delete("/delete", async (req, res) => {
     try {
         const user = UserModel.findOne({ name });
         const deleted_user = await UserModel.findByIdAndDelete({ _id: user._id });
-        res.status(200).send({ "msg": `user with id ${user._id} is deleted successfully`, "deleted_user": deleted_user });
+        res.status(200).send({
+            msg: `user with id ${user._id} is deleted successfully`,
+            deleted_user: deleted_user,
+        });
+    } catch (err) {
+        res.status(500).send({ Error: err.message });
     }
-    catch (err) {
-        res.status(500).send({ "Error": err.message });
-    }
-})
+});
 
 module.exports = { userRouter };
